cb2c6f2f03140a90b2d73524ad7b57bdcd323fb6
quocbao1087 <quocbao1087@gmail.com>
2016-11-26 19:47:54 +0700

    first commit

---
+++ android/build/generated/obj/local/armeabi-v7a/objs/bb.test/bb.test.EgzProxy.o.d
@@ -0,0 +1,184 @@
+/Users/quocbao/Documents/Appcelerator_Studio_Workspace/bbandroidtest/android/build/generated/obj/local/armeabi-v7a/objs/bb.test/bb.test.EgzProxy.o: \
+ jni/bb.test.EgzProxy.cpp jni/bb.test.EgzProxy.h \
+ /Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/jni.h \
+ /Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h \
+ /Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h \
+ /Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stdarg.h \
+ /Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stl/_cprolog.h \
+ /Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stl/config/features.h \
+ /Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h \
+ /Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stl/config/user_config.h \
+ /Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stl/config/compat.h \
+ /Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stl/config/host.h \
+ /Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stl/config/_system.h \
+ /Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stl/config/_android.h \
+ /Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h \
+ /Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h \
+ /Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h \
+ /Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stl/_epilog.h \
+ /Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h \
+ /Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/Proxy.h \
+ /Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/v8.h \
+ /Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/v8stdint.h \
+ /Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stddef.h \
+ /Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stdio.h \
+ /Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/../include/stdio.h \
+ /Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/types.h \
+ /Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdint.h \
+ /Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_types.h \
+ /Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/_types.h \
+ /Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h \
+ /Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stddef.h \
+ /Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/compiler.h \
+ /Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h \
+ /Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/types.h \
+ /Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/types.h \
+ /Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/kernel.h \
+ /Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/JavaObject.h \
+ /Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/assert.h \
+ /Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/../include/assert.h \
+ /Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/EventEmitter.h \
+ /Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/NativeObject.h \
+ /Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/AndroidUtil.h \
+ /Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/log.h \
+ /Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stdlib.h \
+ /Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/../include/stdlib.h \
+ /Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/string.h \
+ /Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/../include/string.h \
+ /Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/malloc.h \
+ /Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/alloca.h \
+ /Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/strings.h \
+ /Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/memory.h \
+ /Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/EventEmitter.h \
+ /Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/JNIUtil.h \
+ /Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/AndroidUtil.h \
+ /Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/JSException.h \
+ /Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/ProxyFactory.h \
+ /Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/TypeConverter.h \
+ /Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/V8Util.h \
+ /Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/org.appcelerator.titanium.proxy.TiViewProxy.h \
+ /Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/Proxy.h
+
+jni/bb.test.EgzProxy.h:
+
+/Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/jni.h:
+
+/Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs.h:
+
+/Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/api-level.h:
+
+/Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stdarg.h:
+
+/Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stl/_cprolog.h:
+
+/Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stl/config/features.h:
+
+/Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h:
+
+/Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stl/config/user_config.h:
+
+/Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stl/config/compat.h:
+
+/Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stl/config/host.h:
+
+/Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stl/config/_system.h:
+
+/Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stl/config/_android.h:
+
+/Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h:
+
+/Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h:
+
+/Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h:
+
+/Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stl/_epilog.h:
+
+/Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h:
+
+/Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/Proxy.h:
+
+/Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/v8.h:
+
+/Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/v8stdint.h:
+
+/Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stddef.h:
+
+/Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stdio.h:
+
+/Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/../include/stdio.h:
+
+/Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/types.h:
+
+/Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/stdint.h:
+
+/Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/_types.h:
+
+/Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/_types.h:
+
+/Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/posix_types.h:
+
+/Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/stddef.h:
+
+/Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/compiler.h:
+
+/Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/posix_types.h:
+
+/Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/asm/types.h:
+
+/Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/linux/types.h:
+
+/Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/machine/kernel.h:
+
+/Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/JavaObject.h:
+
+/Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/assert.h:
+
+/Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/../include/assert.h:
+
+/Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/EventEmitter.h:
+
+/Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/NativeObject.h:
+
+/Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/AndroidUtil.h:
+
+/Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/android/log.h:
+
+/Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/stdlib.h:
+
+/Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/../include/stdlib.h:
+
+/Applications/android-ndk-r10e/sources/cxx-stl/stlport/stlport/string.h:
+
+/Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/../include/string.h:
+
+/Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/malloc.h:
+
+/Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/alloca.h:
+
+/Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/strings.h:
+
+/Applications/android-ndk-r10e/platforms/android-3/arch-arm/usr/include/memory.h:
+
+/Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/EventEmitter.h:
+
+/Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/JNIUtil.h:
+
+/Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/AndroidUtil.h:
+
+/Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/JSException.h:
+
+/Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/ProxyFactory.h:
+
+/Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/TypeConverter.h:
+
+/Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/V8Util.h:
+
+/Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/org.appcelerator.titanium.proxy.TiViewProxy.h:
+
+/Users/quocbao/Library/Application\ Support/Titanium/mobilesdk/osx/5.5.1.GA/android/native/include/Proxy.h:
